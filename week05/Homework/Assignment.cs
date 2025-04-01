ea3022a9c1d8c56306a9304f805fd3d0b91d5dc7
jobrodne <jrcademus87@gmail.com>
2025-04-01 11:01:52 -0500

    HomeworkWeek05

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,22 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+    // Optional: public method to access student name in derived class
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+}
